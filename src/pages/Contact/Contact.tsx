b78f8cbb127239244df562efbe8e49af11dbea29
Vladyslav Maidaniuk <vlad.maidanyuk95@gmail.com>
2023-09-18 17:01:05 -0600

    first commit

---
+++ src/pages/Contact/Contact.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './Contact.css';
+
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Add functionality to handle form submission
+    console.log(formData);
+  };
+
+  return (
+    <section id="contact" className="contact-container">
+      <h2>Contact Me</h2>
+      <form onSubmit={handleSubmit} className="contact-form">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="subject">Subject</label>
+          <input type="text" id="subject" name="subject" value={formData.subject} onChange={handleChange} required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.css'; // Add your styles here
+
+const Home: React.FC = () => {
+  return (
+    <div id="home" className="home-container">
+      <video 
+        autoPlay 
+        loop 
+        muted 
+        className="background-video"
+        poster="/path/to/fallback-image.jpg"
+      ><source src="/videos/portfoliobackground.mp4" type="video/mp4" />
+      Your browser does not support the video tag.</video>
+      <div className="content">
+      <h1>Vladyslav Maidaniuk</h1>
+      <p>Explore my work and contact me for collaborations.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
